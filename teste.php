f1cfe4d5be35484aabe48cad679bccae7c744042
Leidi <leidineiacruz1@gmail.com>
2025-11-16 14:08:55 -0300

    .

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=SUA_SENHA");
-
-if (!$conn) {
-    echo pg_last_error();
-} else {
-    echo "Conectou!";
-}
-?>
